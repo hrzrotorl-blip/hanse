f8a299704802e36bc17154d0bb8c2763e296a334
hrzrotorl-blip <hrzrotorl@gmail.com>
2025-10-22 12:36:17 +0900

    HI

--- Assets/HI.cs
+++ Assets/HI.cs
@@ -7,7 +7,7 @@ public class HI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hI");
     }
 
     // Update is called once per frame
