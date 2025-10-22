a31c15a439e1c8ee92f22421006bc1dd57f8e996
ChoiYiJu <yijuju0621@gmail.com>
2025-10-22 17:09:03 +0900

    헬로우 월드에서 작업

--- Assets/Hi2.cs
+++ Assets/Hi2.cs
@@ -7,7 +7,7 @@ public class Hi2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("헬로우 월드에서 작업한 내용");
     }
 
     // Update is called once per frame
